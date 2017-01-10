b60866b064f7207567e16060fbc851d58b9b7cb3
Lisa Coppinger <lisa.c.coppinger@uk.pwc.com>
2017-01-10 11:57:32 +0000

    animation addition

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+  //even height for homepage sections
+  if ($(window).width() >= 700) {
+    EvenHeightHelper.makeEvenHeight('.homepage-sections');
+  }
+  EvenHeightHelper.makeEvenHeight('.contact__wrapper');
+
+  //initiate wowjs
+  new WOW().init();
+
   //thumbnail flip animation
   $('.hover').hover(function () {
     $(this).addClass('flip');
@@ -7,11 +16,32 @@ $(document).ready(function(){
     $(this).removeClass('flip');
   });
 
-  //even height for homepage sections
-    if ($(window).width() >= 700) {
-      EvenHeightHelper.makeEvenHeight('.homepage-sections');
-    }
-    EvenHeightHelper.makeEvenHeight('.contact__wrapper');
+  //Quick scroll down homepage
+  $('a[href^="#information"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+
+  $('a[href^="#tips"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
 
   //Scroll to top button
   //Check to see if the window is top if not then display button
